aa3f82956170769bcee7183609b5f68c621cbf59
Seb Kay <seb@fhoke.com>
2021-08-19 10:11:19 +0100

    Refactor to using the Tailor class for all CLI commands

--- src/Tailor.js
+++ src/Tailor.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { spawn } = require('child_process');
 const chalk = require('chalk');
 
 class Tailor {
@@ -19,10 +20,106 @@ class Tailor {
             ...this.providerSettings,
         };
 
+        this.webpackCliPath = this.root + '/node_modules/.bin/webpack';
+        this.webpackConfigPath = this.root + '/webpack.config.js';
+
         this.cmdSettings = {
             spacerStart: chalk.gray(`============================== ${this.name} ==============================`),
             spacerEnd: chalk.gray(`====================================================================`),
         };
+    }
+
+    runDevelopment() {
+        let cmd = spawn(
+            this.webpackCliPath,
+            [
+                '--mode=development',
+                `--config=${this.webpackConfigPath}`
+            ],
+            { stdio: "inherit" }
+        );
+
+        this.executeCommand(
+            cmd,
+            chalk.white(`${chalk.magenta('==>')} Compiling assets (${chalk.green('development')})...`),
+            chalk.white(`${chalk.green('\u2714')} Assets compiled (${chalk.green('development')})`)
+        );
+    }
+
+    runProduction() {
+        let cmd = spawn(
+            this.webpackCliPath,
+            [
+                '--mode=production',
+                `--config=${this.webpackConfigPath}`
+            ],
+            { stdio: "inherit" }
+        );
+
+        this.executeCommand(
+            cmd,
+            chalk.white(`${chalk.magenta('==>')} Compiling assets (${chalk.green('production')})...`),
+            chalk.white(`${chalk.green('\u2714')} Assets compiled (${chalk.green('production')})`)
+        );
+    }
+
+    watchDevelopment() {
+        const cmd = spawn(
+            this.webpackCliPath,
+            [
+                '--mode=development',
+                '--watch',
+                `--config=${this.webpackConfigPath}`,
+            ],
+            { stdio: "inherit" }
+        );
+
+        this.executeCommand(
+            cmd,
+            chalk.white(`${chalk.magenta('==>')} Watching assets (${chalk.green('development')})...`)
+        );
+    }
+
+    watchProduction() {
+        const cmd = spawn(
+            this.webpackCliPath,
+            [
+                '--mode=production',
+                '--watch',
+                `--config=${this.webpackConfigPath}`,
+            ],
+            { stdio: "inherit" }
+        );
+
+        this.executeCommand(
+            cmd,
+            chalk.white(`${chalk.magenta('==>')} Watching assets (${chalk.green('production')})...`)
+        );
+    }
+
+    /**
+     * Process for executing commands
+     *
+     * @param {ChildProcessWithoutNullStreams} spawn
+     */
+    executeCommand(spawn, start_message = '', end_message = '') {
+        if (start_message) {
+            spawn.on("spawn", code => {
+                console.log(
+                    `${this.cmdSettings.spacerStart}\n` +
+                    `${start_message}\n`
+                );
+            });
+        }
+
+        if (end_message) {
+            spawn.on("close", code => {
+                console.log(
+                    `\n${end_message}\n` +
+                    `${this.cmdSettings.spacerStart}`
+                );
+            });
+        }
     }
 }
 
--- tailor.js
+++ tailor.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-const Path = require('path');
-const { spawn } = require('child_process');
-const chalk = require('chalk');
 const TailorClass = require('./src/Tailor');
-
 const Tailor = new TailorClass();
 
 const commands = process.argv.slice(2);
@@ -14,81 +10,20 @@ const commands = process.argv.slice(2);
  */
 //----  Webpack devevelopment build
 if (commands.includes('dev')) {
-    const cmd = spawn(Tailor.providerSettings.root + '/node_modules/.bin/webpack', [
-        '--mode=development',
-        `--config=${Path.resolve(Tailor.root, 'webpack.config.js')}`
-    ], { stdio: "inherit" });
-
-    execCommand(
-        cmd,
-        chalk.white(`${chalk.magenta('==>')} Compiling assets (${chalk.green('development')})...`),
-        chalk.white(`${chalk.green('\u2714')} Assets compiled (${chalk.green('development')})`)
-    );
+    Tailor.runDevelopment();
 }
 
 //----  Webpack production build
 if (commands.includes('prod')) {
-    const cmd = spawn(Tailor.providerSettings.root + '/node_modules/.bin/webpack', [
-        '--mode=production',
-        `--config=${Path.resolve(Tailor.root, 'webpack.config.js')}`
-    ], { stdio: "inherit" });
-
-    execCommand(
-        cmd,
-        chalk.white(`${chalk.magenta('==>')} Compiling assets (${chalk.green('production')})...`),
-        chalk.white(`${chalk.green('\u2714')} Assets compiled (${chalk.green('production')})`)
-    );
+    Tailor.runProduction();
 }
 
 //----  Webpack watch for development changes
 if (commands.includes('watch-dev')) {
-    const cmd = spawn(Tailor.providerSettings.root + '/node_modules/.bin/webpack', [
-        '--mode=development',
-        '--watch',
-        `--config=${Path.resolve(Tailor.root, 'webpack.config.js')}`,
-    ], { stdio: "inherit" });
-
-    execCommand(
-        cmd,
-        chalk.white(`${chalk.magenta('==>')} Watching assets (${chalk.green('development')})...`)
-    );
+    Tailor.watchDevelopment();
 }
 
 //----  Webpack watch for production changes
 if (commands.includes('watch-prod')) {
-    const cmd = spawn(Tailor.providerSettings.root + '/node_modules/.bin/webpack', [
-        '--mode=production',
-        '--watch',
-        `--config=${Path.resolve(Tailor.root, 'webpack.config.js')}`
-    ], { stdio: "inherit" });
-
-    execCommand(
-        cmd,
-        chalk.white(`${chalk.magenta('==>')} Watching assets (${chalk.green('production')})...`)
-    );
-}
-
-/**
- * Process for executing commands
- *
- * @param {ChildProcessWithoutNullStreams} spawn
- */
-function execCommand(spawn, start_message = '', end_message = '') {
-    if (start_message) {
-        spawn.on("spawn", code => {
-            console.log(
-                `${Tailor.cmdSettings.spacerStart}\n` +
-                `${start_message}\n`
-            );
-        });
-    }
-
-    if (end_message) {
-        spawn.on("close", code => {
-            console.log(
-                `\n${end_message}\n` +
-                `${Tailor.cmdSettings.spacerStart}`
-            );
-        });
-    }
+    Tailor.watchProduction();
 }
